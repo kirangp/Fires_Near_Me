940f3f731375f83b049a85a48d822ee712af7cdf
kirangp <kirangp@gmail.com>
2016-04-21 23:35:06 -0400

    Initial commit. All files created

---
+++ lib/Fires_Near_Me.rb
@@ -0,0 +1,6 @@
+require "Fires_Near_Me/version"
+require_relative "./Fires_Near_Me/cli" 
+
+module FiresNearMe
+  # Your code goes here...
+end
---
+++ lib/Fires_Near_Me/version.rb
@@ -0,0 +1,3 @@
+module FiresNearMe
+  VERSION = "0.1.0"
+end
